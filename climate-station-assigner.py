4ed46e6525aba2a41e2277eea4f281047027de89
Ana Constantinescu <anaconst140@gmail.com>
2025-07-10 21:36:40 -0700

    cleaning up

--- climate-station-assigner.py
+++ climate-station-assigner.py
@@ -5,22 +5,18 @@ import math
 from owslib.ogcapi.features import Features
 from datetime import date
 
+#Outputs a CSV of Canadian cities and their respective weather stations.
+
 buffer = 7 #km radius for weather stations 
 
 # ESPG code of the preferred projection to create the buffer
 # NAD83 / Statistics Canada Lambert
 projection = 3347
 
-latitude_to_km = 110.574 #1deg latitude in km
-longitude_to_km = 111.320 #*cos(latitude), 1 deg long in km
-
+#Possibly temporary, until I decide where stations should be sorted by date
 start_date = date(2000, 1, 1)
 end_date = date(2020, 8, 31)
 time_ = f"{start_date}/{end_date}"
-
-#1st: find all stations located within a town; 5x5 bounding box
-#(convert from lat/long distance to km)
-#2nd: when a city is selected, for each station it is attached to, create a new .csv that stores id of city, and calculates daily average temperature recorded for a given day and a given date range
 
 def km_to_deg(lat, km_lat, km_lon):
     deg_lat = km_lat / 111.0
@@ -28,12 +24,10 @@ def km_to_deg(lat, km_lat, km_lon):
     return deg_lat, deg_lon
 
 cities = pd.read_csv("simplemaps_canadacities_basic/canadacities.csv")
-print(cities.head())
 for index, city in cities.iterrows():
     city_lat = city['lat']
     city_long = city['lng']
     province = city["province_id"]
-
 
     deg_lat, deg_lon = km_to_deg(city_lat, buffer, buffer)
     bbox = [
@@ -51,18 +45,15 @@ for index, city in cities.iterrows():
     # Verification of the retrieved data
     if "features" in station_data:
         station_data = json.dumps(station_data, indent=4)
-        # for station in station_data["features"]:
-        #     props = station["properties"]
-        #     identifier = props["CLIMATE_IDENTIFIER"]
-        #     print(identifier)
+
     else:
         raise ValueError(
-            "No hydrometric stations were found. Please verify the coordinates."
+            "No weather stations were found. Please verify the coordinates."
         )
     
     # List of stations located inside the buffer zone
 
-    # Accessing the hydrometric stations layer
+    # Accessing the weather stations layer
     driver = ogr.GetDriverByName("GeoJSON")
     data_source = driver.Open(station_data, 0)
     layer = data_source.GetLayer()
@@ -77,7 +68,7 @@ for index, city in cities.iterrows():
     SRS_projected = osr.SpatialReference()
     SRS_projected.ImportFromEPSG(projection)
 
-# Transformation from input SRS to the prefered projection
+    # Transformation from input SRS to the prefered projection
     transform = osr.CoordinateTransformation(SRS_input, SRS_projected)
 
     # Creation of a buffer to select stations
@@ -86,7 +77,7 @@ for index, city in cities.iterrows():
     point.Transform(transform)
     point_buffer = point.Buffer(10 * 1000)  # The value must be in meters
 
-    # Selection of the stations in the buffer zone
+    # Selection of the station names & climate identifiers in the buffer zone
     stations = []
     identifiers = []
 
@@ -94,7 +85,7 @@ for index, city in cities.iterrows():
         geom = feature.GetGeometryRef().Clone()
         geom.Transform(transform)
         if geom.Intersects(point_buffer):
-            if(feature.PROV_STATE_TERR_CODE == province):
+            if(feature.PROV_STATE_TERR_CODE == province): #Extra check that might be unnecessary now
                 stations.append(feature.STATION_NAME)
                 identifiers.append(feature.CLIMATE_IDENTIFIER)
 
@@ -106,7 +97,7 @@ for index, city in cities.iterrows():
     cities.at[index, 'station_names'] = stations
     cities.at[index, 'station_ids'] = identifiers
 
-    # Raising an error if no station were found
+    # Print output whenever there are no stations
     if not stations:
         print (
             f"There are no climate stations within {buffer} km"
